5eac13d08664deb5ee8bc9b2299348744da30be6
Justin Singh <chaosdoggs@Justins-MacBook-Pro.local>
2018-09-10 14:46:22 -0700

    you are going somehwere

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,84 +15,44 @@ $(document).ready(function () {
 
 
 
+    //an onclick to get the buttons
+    $('.choose-characters').on('click', function () {
 
-    $('#character-1').on('click', function () {
+        //check if certain characters are clicked
+        if (!game.isCharacterChosen) {
+            if (this.id == 'character-1') {
+                //move character 1 to chosen character
+                $('#your-character').append($('#character-1'));
+                game.isCharacterChosen = true;
+            }
+            if (this.id == 'character-2') {
+                //move character 1 to chosen character
+                $('#your-character').append($('#character-2'));
+                game.isCharacterChosen = true;
 
-        if (game.isCharacterChosen && game.isFighterChosen) {
+            }
+            if (this.id == 'character-3') {
+                //move character 1 to chosen character
+                $('#your-character').append($('#character-3'));
+                game.isCharacterChosen = true;
+
+            }
+            if (this.id == 'character-4') {
+                //move character 1 to chosen character
+                $('#your-character').append($('#character-4'));
+                game.isCharacterChosen = true;
+
+            }
 
         }
-        if (game.isCharacterChosen === true) {
-            $('#defender').append($('#character-1'));
-            game.isFighterChosen = true;
-        }
-        else {
-            $('#your-character').append($('#character-1'));
-            $('#enemies-to-attack').append($('#character-2'));
-            $('#enemies-to-attack').append($('#character-3'));
-            $('#enemies-to-attack').append($('#character-4'));
-            game.isCharacterChosen = true;
-
-        }
-
-
-    })
-    $('#character-2').on('click', function () {
-
-        if (game.isCharacterChosen === true) {
-            $('#defender').append($('#character-2'));
-            game.isFighterChosen = true;
-        }
-        else {
-            $('#your-character').append($('#character-2'));
-            $('#enemies-to-attack').append($('#character-1'));
-            $('#enemies-to-attack').append($('#character-3'));
-            $('#enemies-to-attack').append($('#character-4'));
-            game.isCharacterChosen = true;
-        }
-
-
-    })
-
-    $('#character-3').on('click', function () {
-        if (game.isCharacterChosen === true) {
-            $('#defender').append($('#character-3'));
-            game.isFighterChosen = true;
-        }
-        else {
-            $('#your-character').append($('#character-3'));
-            $('#enemies-to-attack').append($('#character-1'));
-            $('#enemies-to-attack').append($('#character-2'));
-            $('#enemies-to-attack').append($('#character-4'));
-            game.isCharacterChosen = true;
+        //if character is chosen but fighter is not chosen do this
+        if ((game.isCharacterChosen) && (!game.isFighterChosen)) {
+            if (this.id == 'character-1') {
+                $('#enemies-to-attack').append($('#character-2'));
+                $('#enemies-to-attack').append($('#character-3'));
+                $('#enemies-to-attack').append($('#character-4'));
+            }
         }
 
     })
-    $('#character-4').on('click', function () {
-        if (game.isCharacterChosen === true) {
-            $('#defender').append($('#character-4'));
-            game.isFighterChosen = true;
-        }
-        else {
-            $('#your-character').append($('#character-4'));
-            $('#enemies-to-attack').append($('#character-1'));
-            $('#enemies-to-attack').append($('#character-2'));
-            $('#enemies-to-attack').append($('#character-3'));
-            game.isCharacterChosen = true;
-
-        }
-
-    })
-
-
-    if (game.isCharacterChosen === true) {
-
-    }
-
-
-
-
-
-
-
-
 })
